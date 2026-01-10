526d60336e7b71ac9dec67423975edc2c2d03e09
Kloym <oleg.petr1223@gmail.com>
2026-01-10 22:06:37 +0300

    k

--- server/server.py
+++ server/server.py
@@ -950,4 +950,5 @@ admin.add_view(MessageView(MessageModel, db_orm.session, name="Сообщени�
 if __name__ == "__main__":
     with app.app_context():
         init_db()
+    port = int(os.environ.get("PORT", 10000))
     app.run(host="0.0.0.0", port=5000, debug=False)
