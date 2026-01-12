c5ed5e49502c3e3fca12ee00e29b977bbf376727
Kloym <oleg.petr1223@gmail.com>
2026-01-12 12:26:54 +0300

    Deploy: switch to persistent storage /data

--- server/server.py
+++ server/server.py
@@ -951,4 +951,4 @@ if __name__ == "__main__":
     with app.app_context():
         init_db()
     port = int(os.environ.get("PORT", 10000))
-    app.run(host="0.0.0.0", port=5000, debug=False)
+    app.run(host="0.0.0.0", port=port, debug=False)
